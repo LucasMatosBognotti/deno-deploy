12055bcd067014bee35871d8bcfe60c840218270
Lucas Matos Bognotti <lucasmatosbognotti@gmail.com>
2021-07-01 08:59:10 -0300

    add oak

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hello World Sonig blading!", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = "Hello World OAK!";
 });
+
+addEventListener("fetch", app.fetchEventHandler());
